ce85ddbc36a66a1dd2ac89b026290408f24d178f
chinnu2010 <111685968+chinnu2010@users.noreply.github.com>
2022-11-23 18:43:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+
+difference=0;
+rightWristX=0;
+leftWristX=0;
+function setup () {
+    video=createCapture(VIDEO);
+    video.size (550, 500);
+
+    canvas = createCanvas(550,550);
+    canvas.position (560,150);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function draw() {
+    document.getElementById("font_size").innerHTML=" font size of the text will be "+difference+"px";
+    background('#969A97');
+fill('#F90093');
+textSize(difference)
+text("Bhagya", 50,400);
+}
+function modelLoaded () {
+    console.log('PoseNet Is Initialized!')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        
+        leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+        difference= floor(leftWristX-rightWristX);
+        console.log("leftWristX=" +leftWristX+"rightWristX=" +rightWristX+ "difference="+difference);    
+}  
+}   
+    
+    
+    
